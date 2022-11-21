3c26866e9fbf60e87242b18f0a244e331d4acc3e
Kavin Phan <kavin@kphan.tech>
2022-11-20 23:10:18 -0500

    Add config to enable cors everywhere

---
+++ src/main/java/com/ttertweets/demo/config/WebConfiguration.java
@@ -0,0 +1,14 @@
+package com.ttertweets.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**").allowedMethods("*");
+  }
+}
